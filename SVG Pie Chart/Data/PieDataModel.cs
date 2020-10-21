f574aad4f3e621a859d163ff3126bf7a587d3deb
Mariano J. Padilla <mariano.padilla@outlook.com>
2020-10-21 17:32:25 -0600

    Add project files.

---
+++ SVG Pie Chart/Data/PieDataModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SVG_Pie_Chart.Data
+{
+    public class PieDataModel
+    {
+        public double Percent { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
